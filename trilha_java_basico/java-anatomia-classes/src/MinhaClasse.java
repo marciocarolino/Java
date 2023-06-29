941227edd33c6ef3529c3a05664c637b402b5cfb
Marcio Carolino <marciocarolino@gmail.com>
2023-06-29 11:25:43 -0300

    continuação dos estudos

--- trilha_java_basico/java-anatomia-classes/src/MinhaClasse.java
+++ trilha_java_basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,15 @@
 public class MinhaClasse {
 
   public static void main(String args[]) {
-    System.out.println("Bem - Vindo Java!");
+    String primeioNome = "Marcio";
+    String segundoNome = "Carolino";
+
+    String nomeCompleto = nomeCompleto(primeioNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+  }
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
   }
 }
